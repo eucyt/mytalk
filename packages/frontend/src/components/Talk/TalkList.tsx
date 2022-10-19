16815fa58387ca4e6cc1a053f7549343b5361aee
yuta <eypc.ofun@gmail.com>
2022-10-19 21:34:44 +0900

    fix if no talks

--- packages/frontend/src/components/Talk/TalkList.tsx
+++ packages/frontend/src/components/Talk/TalkList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Item from "@/components/Common/Item";
 import List from "@/components/Common/List";
 import CreateTalkButton from "@/components/Talk/CreateTalkButton";
 import TaskListItem from "@/components/Talk/TalkListItem";
@@ -21,16 +22,24 @@ const TalkList: React.FC<Props> = (props) => {
           {/*</div>*/}
         </>
       }
-      items={props.talks?.map((talk, i) => {
-        return (
-          <TaskListItem
-            id={talk.id}
-            talkMemberNames={talk.users
-              .map((user) => user.name)
-              .filter((v) => v !== props.username)}
-          />
-        );
-      })}
+      items={
+        props.talks?.length !== 0 ? (
+          props.talks?.map((talk, i) => {
+            return (
+              <TaskListItem
+                id={talk.id}
+                talkMemberNames={talk.users
+                  .map((user) => user.name)
+                  .filter((v) => v !== props.username)}
+              />
+            );
+          })
+        ) : (
+          <Item>
+            <p>No Talks...</p>
+          </Item>
+        )
+      }
       outItems={
         <div className="absolute bottom-0 right-0 mr-2">
           <CreateTalkButton />
