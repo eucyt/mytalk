4126e2ed5674a97a061c067f772277562824b03d
yuta <eypc.ofun@gmail.com>
2022-10-27 00:22:04 +0900

    fix talk page

--- packages/frontend/src/pages/talks/[id].tsx
+++ packages/frontend/src/pages/talks/[id].tsx
@@ -1,21 +1,19 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import React from "react";
 
 import AuthLayout from "@/components/Common/Layouts/AuthLayout";
-import MessageList from "@/components/Talk/MessegeList";
-import SendingMessageArea from "@/components/Talk/SendingMessageArea";
-import TalkHeader from "@/components/Talk/TalkHeader";
 
 const Index = () => {
-  const router = useRouter();
-  const { id } = router.query;
+  // const router = useRouter();
+  // const { id } = router.query;
 
   return (
     <AuthLayout title="MyTalk - Message">
       <div className="flex h-screen flex-1 flex-col justify-between sm:p-6">
-        <TalkHeader talkId={Number(id)} />
-        <MessageList />
-        <SendingMessageArea />
+        <p>Coming Soon...</p>
+        {/*<TalkHeader talkId={Number(id)} />*/}
+        {/*<MessageList />*/}
+        {/*<SendingMessageArea />*/}
       </div>
     </AuthLayout>
   );
