75a4681033892fafef92205996e677fdff284d9d
yuta <eypc.ofun@gmail.com>
2022-10-26 21:30:38 +0900

    fix cors

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -7,7 +7,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ forbidNonWhitelisted: true })); // for class-validator
-  app.enableCors({ origin: ['http://localhost:8080'] });
+  app.enableCors({
+    origin: ['http://localhost:8080', 'https://mytalk.euchi.jp'],
+  });
   await app.listen(3000);
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
